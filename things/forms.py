0efc71146344c1ec191ba2aad9b2a68f9c03e691
lukasanader <lukasanader@hotmail.com>
2023-11-05 11:56:05 +0000

    jnin

--- things/forms.py
+++ things/forms.py
@@ -6,6 +6,6 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model =Thing
         fields=['name','description','quantity']
-    name = forms.CharField(type='text',max_length=35)
-    description=forms.Textarea(widget=forms.Textarea)
+    #name = forms.CharField(type='text',max_length=35)
+    #description=forms.Textarea(widget=forms.Textarea)
     quantity=forms.NumberInput()
